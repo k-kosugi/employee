69b47beeca012d8e462a9809f8392dbf70fc9563
kentak <kenta.kosugi@gmail.com>
2019-06-11 21:02:52 +0900

    Change some WebAPI method of EmployeeResource.

--- src/main/java/org/kenta/kosugi/employee/api/EmployeeResource.java
+++ src/main/java/org/kenta/kosugi/employee/api/EmployeeResource.java
@@ -29,17 +29,33 @@ public class EmployeeResource {
      */
     @GET
     @Path("/all")
-    public List<Employee> all(){
+    public List<Employee> all() {
         return this.em.createNamedQuery("Employee.findAll", Employee.class)
                 .getResultList();
     }
 
+    /**
+     * Find employee object with using employee id.
+     *
+     * @param id employee id.
+     * @return Return employee object with using employee id.
+     */
     @GET
     @Path("/find/{id}")
-    public Employee find(@PathParam("id") String id){
+    public Employee find(@PathParam("id") String id) {
         return this.em.find(Employee.class, id);
     }
 
+    /**
+     * Regist the employee object to EMPLOYEE table.
+     *
+     * @param id         Employee id.
+     * @param firstName  First Name.
+     * @param middleName Middle Name.
+     * @param lastName   Last Name.
+     * @param hiredDate  Hired Date.
+     * @return Return 200 OK response when request success.
+     */
     @POST
     @Path("/regist/{id}")
     public Response registEmployee(
@@ -47,10 +63,11 @@ public class EmployeeResource {
             @QueryParam("firstName") String firstName,
             @QueryParam("middleName") String middleName,
             @QueryParam("lastName") String lastName,
-            @QueryParam("hireDate") String hireDate){
+            @QueryParam("hiredDate") String hiredDate) {
 
         try {
-            Employee employee = new Employee(id, firstName, middleName, lastName, hireDate);
+            // Find the employee object by using employee id.
+            Employee employee = new Employee(id, firstName, middleName, lastName, hiredDate);
 
             this.em.persist(employee);
         } catch (ParseException e) {
@@ -60,22 +77,33 @@ public class EmployeeResource {
         return Response.ok().build();
     }
 
+    /**
+     * Regist the boss object to employee object.
+     *
+     * @param id     Employee id.
+     * @param bossId Boss's employee id.
+     * @return Return the 200 OK response when regist request success.
+     * Return the 404 NG response when regist request fail.
+     */
     @PUT
     @Path("/regist/{id}/boss")
     public Response registBoss(
-            @PathParam("id") String id, @QueryParam("bossId") String bossId){
+            @PathParam("id") String id, @QueryParam("bossId") String bossId) {
 
+        // Find the employee object by using employee id.
         Employee employee = this.em.find(Employee.class, id);
-        if(employee == null){
+        if (employee == null) {
             return Response.status(404).encoding("Can't found employee with employeeid = " + id).build();
         }
 
+        // Find the boss object by using boss's employee id.
         Employee boss = this.em.find(Employee.class, bossId);
-        if(boss == null){
+        if (boss == null) {
             return Response.status(404).encoding("Can't found employee with bossId = " + boss).build();
         }
 
-        employee.boss = boss;
+        // Set boss object to employee object.
+        employee.setBoss(boss);
 
         this.em.merge(employee);
 
@@ -83,18 +111,32 @@ public class EmployeeResource {
 
     }
 
+    /**
+     * Delete the employee object from this EMPLOYEE table.
+     * Set the leaved date to this employee object.
+     *
+     * @param id         employee id
+     * @param leavedDate Leaved Date.
+     * @return Return the 200 OK response when the request success.
+     * If response fail, return 400 response.
+     */
     @DELETE
     @Path("/leave/{id}")
-    public Response leave(@PathParam("id") String id, @QueryParam("leavedDate") String leavedDate){
+    public Response leave(@PathParam("id") String id, @QueryParam("leavedDate") String leavedDate) {
+
+        // Find employee object by id.
         Employee employee = this.em.find(Employee.class, id);
         try {
-            employee.setHireDate(leavedDate);
+            employee.setLeavedDate(leavedDate);
         } catch (ParseException e) {
             return Response.status(400).encoding(e.toString()).build();
         }
 
+        // merge employee object
         this.em.merge(employee);
 
+        // Return 200 response code.
         return Response.ok().build();
+
     }
 }
--- src/main/java/org/kenta/kosugi/employee/model/Employee.java
+++ src/main/java/org/kenta/kosugi/employee/model/Employee.java
@@ -23,52 +23,156 @@ public class Employee implements Serializable {
 
     private static final long serialVersionUID = -7763827188716065700L;
 
+    /**
+     * Primary key for this EMPLOYEE table.
+     */
     @Id
     @Column(name = "ID", length = 8)
     public String id;
 
+    /**
+     * First Name associated with this employee object.
+     */
+    private String firstName;
+
+    /**
+     * Get the first name associated with this employee object.
+     *
+     * @return The first name associated with this employee object.
+     */
     @Column(name = "FIRST_NAME", length = 32)
-    public String firstName;
+    public String getFirstName() {
+        return this.firstName;
+    }
 
+    /**
+     * Set first name to employee object.
+     *
+     * @param firstName First name associated with this employee object.
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private String middleName;
+
+    /**
+     * Get Middle name associated with this employee.
+     *
+     * @return Middle name associated with this employee.
+     */
     @Column(name = "MIDDLE_NAME", length = 32, nullable = true)
-    public String middleName;
+    public String getMiddleName() {
+        return this.middleName;
+    }
 
+    /**
+     * Set Middle name to this employee.
+     */
+    public void setMiddleName() {
+        this.middleName = middleName;
+    }
+
+    private String lastName;
+
+    /**
+     * Get Last name associated with this employee object.
+     *
+     * @return Last name associated with this employee object.
+     */
     @Column(name = "LAST_NAME", length = 32)
-    public String lastName;
+    public String getLastName() {
+        return this.lastName;
+    }
 
+    public void setLastName() {
+        this.lastName = lastName;
+    }
+
+    private Employee boss;
+
+    /**
+     * Return the boss object associated with this employee.
+     *
+     * @return The boss object associated with this employee.
+     */
     @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    public Employee boss;
+    public Employee getBoss() {
+        return this.boss;
+    }
+
+    /**
+     * Set boss object to this employee.
+     *
+     * @param boss The boss object to this employee.
+     */
+    public void setBoss(Employee boss) {
+        this.boss = boss;
+    }
 
     private Calendar hireDate;
 
-    public void setHireDate(String hireDate) throws ParseException {
+    /**
+     * Set the hired date associated with this employee.
+     *
+     * @param hireDate Hired Date.
+     * @throws ParseException
+     */
+    public void setHiredDate(String hireDate) throws ParseException {
         this.hireDate = this.parse(hireDate);
     }
 
+    /**
+     * Get the hired Date associated with this employee.
+     *
+     * @return Hire Date associated with this employee.
+     */
     @Temporal(TemporalType.DATE)
     @Column(name = "HIRE_DATE", nullable = false)
     public Calendar getHireDate() {
         return hireDate;
     }
 
+    /**
+     * The leaved Date.
+     */
     private Calendar leavedDate;
 
+    /**
+     * Set leaved date to employee object.
+     *
+     * @param leavedDate Leaved Date.
+     * @throws ParseException Throw new ParseException when casting the String to Calendar object.
+     */
     public void setLeavedDate(String leavedDate) throws ParseException {
         this.leavedDate = this.parse(leavedDate);
     }
 
+    /**
+     * Get the leaved Date associated with this employee.
+     *
+     * @return Leaved Date associated with this employee.
+     */
     @Temporal(TemporalType.DATE)
     @Column(name = "LEAVED_DATE")
-    public Calendar getLeavedDate(){
+    public Calendar getLeavedDate() {
         return this.leavedDate;
     }
 
     @Transient
     private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
 
+    /**
+     * Cast String object to Calendar object.
+     *
+     * @param date The date of String for example "20190901"
+     * @return Return the Calendar object.
+     * @throws ParseException
+     */
     @Transient
-    private Calendar parse(String date) throws ParseException{
-        if(date == null){
+    private Calendar parse(String date) throws ParseException {
+
+        if (date == null) {
             return null;
         }
 
@@ -77,6 +181,7 @@ public class Employee implements Serializable {
         cal.setTime(simpleDateFormat.parse(date));
 
         return cal;
+
     }
 
     /**
@@ -87,17 +192,26 @@ public class Employee implements Serializable {
 
     /**
      * Constructor for Employee class without boss object.
-     * @param id Company id.
-     * @param firstName First Name.
+     *
+     * @param id         Company id.
+     * @param firstName  First Name.
      * @param middleName Middle Name.
-     * @param lastName Last Name.
-     * @param hireDate Hired Date.
+     * @param lastName   Last Name.
+     * @param hireDate   Hired Date.
      */
     public Employee(String id, String firstName, String middleName, String lastName, String hireDate) throws ParseException {
+
         this.id = id;
         this.firstName = firstName;
-        this.middleName = middleName;
+
+        if (!"".equals(middleName)) {
+            // set the null when middleName is empty.
+            this.middleName = middleName;
+        }
+
         this.lastName = lastName;
-        this.setHireDate(hireDate);
+        this.setHiredDate(hireDate);
+
     }
+
 }
